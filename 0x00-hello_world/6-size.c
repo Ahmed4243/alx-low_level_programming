3e6b7e3d319e0dbbbff7d5b10b1a14e2edb236fe
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-03-16 12:20:24 -0700

    hey

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * no parameters
+ *
+ * Description: prints something
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of char: %ld byte\n", sizeof(char));
+   	printf("Size of int: %ld bytes\n", sizeof(int));
+    	printf("Size of float: %ld bytes\n", sizeof(float));
+   	printf("Size of double: %ld bytes\n", sizeof(double));
+	return (0);
+}
