c42e95248020221134c9bbb0372abbd6ae2988c6
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-03-16 11:42:25 -0700

    hey

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main: Entry point
+ * no paramters
+ *
+ * Description: Prints \" Programming is like building a mutlilingual puzzle
+ * Returns 0
+ */ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
