53a8ac0fb4328452179bfd97475b49711a7a73c1
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-03-16 11:55:55 -0700

    hey

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  * no parameters
  *
  * Description: with proper grammar, but the outcome is a piece of art,
- * Returns 0
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
