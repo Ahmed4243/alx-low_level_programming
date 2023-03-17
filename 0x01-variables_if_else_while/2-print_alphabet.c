9b6e58a4d446af0e381a7b7a332d40b809466bbc
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-03-17 05:35:21 -0700

    hey

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * no parameters
+ *
+ * Description: prints lowercases characters
+ * Return: 0
+ */
+ int main(void)
+ {
+ 	for(char c = 'a'; c <= 'z'; c++)
+		putchar(c);
+ 	putchar('\n');
+	return (0);
+ }
