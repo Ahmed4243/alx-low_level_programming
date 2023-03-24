c6d9355566b9413240543161fd7e091e3906304b
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-03-23 17:03:44 -0700

    hey

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,28 +1,11 @@
-/*
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
+#ifndef MAIN_H
+#define MAIN_H
 
-int largest_number(int a, int b, int c)
-{
-    int largest;
+#include <stdio.h>
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
-    return (largest);
-}
-
+#endif /* MAIN_H */
