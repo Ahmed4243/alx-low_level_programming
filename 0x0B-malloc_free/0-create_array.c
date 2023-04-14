037564d94312d044bd009e9851d6f64d33dab0fd
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-04-13 19:49:38 -0700

    hey

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * allocates memory
+ * *create_array - allocates memory
  * 2 parameter
  *
  * Description: creates array and allocates memory
@@ -18,12 +18,11 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	ar = malloc(size * sizeof(char));
-	
 	if (ar == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 		ar[i] = c;
-	return (ar);
-		
+
+	return (ar);	
 }
