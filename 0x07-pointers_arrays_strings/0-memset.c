4d85a6619da66a96244ac02c50a6d4732a926d32
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-04-21 07:25:12 -0700

    hey

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - function
+ * 3 parameters
+ *
+ * description: nothing
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)int main(void)
+{
+	*s = b;
+	return (s);
+}
