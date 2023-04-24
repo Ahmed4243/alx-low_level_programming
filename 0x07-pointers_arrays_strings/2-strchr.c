2caffd63ec64e9d96452e353d890dbaecc6350d5
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-04-24 05:56:36 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * length - returns the length of a string
+ * @s: the string it receieves
+ * Description: bla bla
+ * Return: length
+ */
+
+int length(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * *_strchr - finds a character in an array
+ * @s: the string it receives
+ * @c: the character its looking for
+ * Description: bla bla
+ * Return: s or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int len = length(s);
+	int n;
+
+	for (n = 0; n < len; n++)
+	{
+		if (s[n] == c)
+			return (s);
+	}
+
+	return (NULL);
+}
