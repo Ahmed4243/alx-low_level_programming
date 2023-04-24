8e47488c6a6054cb65c8cab7ae63884b33ba180d
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-04-24 06:12:49 -0700

    Fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * length - returns the length of a string
- * @s: the string it receieves
- * Description: bla bla
- * Return: length
- */
-
-int length(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
  * *_strchr - finds a character in an array
  * @s: the string it receives
  * @c: the character its looking for
@@ -32,12 +12,14 @@ char *_strchr(char *s, char c)
 {
 	int len = length(s);
 	int n;
-
-	for (n = 0; n < len; n++)
+	
+	for (n = 0; s[n] != '\0'; n++)
 	{
 		if (s[n] == c)
-			return (s);
+			return (&s[n]);
+		else if (s[n] == '\0')
+			return (0);
 	}
 
-	return ('\0');
+	return (0);
 }
