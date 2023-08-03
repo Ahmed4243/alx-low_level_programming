1b2e4779aa984ac21657502e14519640b8f2ada4
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-08-03 14:09:36 -0700

    idk

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,17 +2,15 @@
 
 /**
  * _strlen_recursion - Returns the length of a given string
- * @s: String whose length is returned
+ * @s: The given string
  *
- * Return: Length of string
+ * Return: Length of the string
  * Description - :)
  */
 
 int _strlen_recursion(char *s)
 {
-
 	if (*s == '\0')
 		return (0);
 	return (1 + _strlen_recursion(s + 1));
-
 }
