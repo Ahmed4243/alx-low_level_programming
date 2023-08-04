dd48c0cffc03a9b2b19dfa42437f32034e88f19b
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-08-04 15:54:57 -0700

    Hey

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,10 @@
-#!/bin/sh -e
-gcc -c *.c
-ar cr liball.a *.o
+#!/bin/sh
+
+# Create a list of all the .c files in the current directory
+c_files=$(find . -name "*.c")
+
+# Compile all the .c files into object files
+gcc -c -Wall -Werror -Wextra $c_files
+
+# Create a static library called liball.a from the object files
+ar -rcs liball.a *.o
