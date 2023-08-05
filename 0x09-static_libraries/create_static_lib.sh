8ec649b06d4c42e2c0f6503fe03fb144f0e14c31
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-08-05 01:17:38 -0700

    Task 2 fix

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,4 @@
-#!/bin/sh
-
-# Create a list of all the .c files in the current directory
-c_files=$(find . -name "*.c")
-
-# Compile all the .c files into object files
-gcc -c -Wall -Werror -Wextra $c_files
-
-# Create a static library called liball.a from the object files
-ar -rcs liball.a *.o
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
