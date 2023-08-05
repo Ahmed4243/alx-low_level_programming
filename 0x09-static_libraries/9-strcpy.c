1ddf0b0eacbe33e6937049b8277fe3e51bda6f32
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-08-05 06:41:53 -0700

    Added directives

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * _strcpy - copies the string pointed to by src,
  * including the terminating null byte, to the
@@ -8,6 +8,7 @@
  * @src: source.
  * Return: the pointer to dest.
  */
+
 char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
