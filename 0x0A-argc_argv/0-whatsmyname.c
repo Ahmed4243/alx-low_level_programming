7e17e39dd4d02fa91e0881f69769e2269bb921cc
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-08-07 16:58:54 -0700

    Task 1 Fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,11 @@
  * @argc: Number of command line arguments
  * @argv: The command line arguments
  *
- * Return: The number of arguments
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	return (argc);
+	printf("%s\n", argv[argc*0]);
+	return (0);
 }
