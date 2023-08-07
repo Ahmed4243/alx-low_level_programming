488d8021a636c6af5eed6a731730c279385decdb
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-08-07 16:51:15 -0700

    Task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the code
+ * @argc: Number of command line arguments
+ * @argv: The command line arguments
+ *
+ * Return: The number of arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
