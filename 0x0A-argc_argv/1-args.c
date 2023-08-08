b7a02d282e6e6352995bcb39f8d4340ab3b619db
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-08-07 17:30:16 -0700

    Checks for argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -13,12 +13,13 @@ int main(int argc, char *argv[])
 {
 	int count;
 
-	if (argc == 1 )
-		printf("%d", argc - 1);
+	if (argc == 1)
+		printf("%d\n", argc - 1);
 	else
 	{
-		for (count = 0; *argv; count++, argv++)
-		      printf("%d", count - 1);	
+		for (count = 0; *argv; count++)
+			argv++;
+		printf("%d\n", count - 1);	
 	}
 	return (0);
 }
