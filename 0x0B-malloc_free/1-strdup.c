fb6094380a5744fc6a1fee60a95f8294ba33c11f
Replit user <>
2023-08-09 23:04:35 +0000

    Task 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *str)
+{
+	int length;
+
+	for (length = 0; str[length] != '\0'; length++)
+		{
+
+		}
+	return (length + 1);
+}
+
+/**
+ * _strdup - Function that returns a pointer to a newly allocated space in memory
+ * @str: String getting copied
+ *
+ * Return: *str
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int size;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	size = _strlen(str);
+	dup = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		if (dup == NULL)
+			return (NULL);
+		dup[i] = str[i];	
+	}
+	return (dup);
+}
