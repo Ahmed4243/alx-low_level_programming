be7687cba664cc50812d72426dde41ec18a6e329
Replit user <>
2023-08-10 01:12:20 +0000

    Betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,22 +18,22 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-    arr = malloc(height * sizeof(int *));
-    if (arr == NULL)
-			return (NULL);
+	arr = malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
 
-    for (x = 0; x < height; x++)
-    {
-			arr[x] = malloc(width * sizeof(int));
-			if (arr[x] == NULL)
-			{
-				for (y = 0; y < x; y++)
-					free(arr[y]);
-				free(arr);
-				return (NULL);
-			}
-			for (y = 0; y < width; y++)
-				arr[x][y] = 0;
-    }
-    return (arr);
+	for (x = 0; x < height; x++)
+	{
+		arr[x] = malloc(width * sizeof(int));
+		if (arr[x] == NULL)
+		{
+			for (y = 0; y < x; y++)
+				free(arr[y]);
+			free(arr);
+			return (NULL);
+		}
+		for (y = 0; y < width; y++)
+			arr[x][y] = 0;
+	}
+	return (arr);
 }
