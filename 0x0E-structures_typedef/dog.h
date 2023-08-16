5b2771f1d876220bcb5c34fc3ec28559432387bf
Replit user <>
2023-08-16 01:10:30 +0000

    Added dog description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+  * struct dog - Dog structure
+  * @name: The dog's name.
+  * @age: The dog's age.
+  * @owner: The dog's owner.
+  *
+  * Description: The attributes of a dog.
+  */
+
 struct dog 
 {
 	char *name;
 	float age;
 	char * owner;
 };
+
 #endif
